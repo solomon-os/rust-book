958c1e47cea029c56459119f60da5e5dbdf612fa
Segun Oladiran <sheghunoladiran9@gmail.com>
2026-07-12 19:02:27 +0100

    chapter 2 done

---
+++ chapter_2/src/main.rs
@@ -0,0 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+fn main() {
+    println!("Guess the number");
+    println!("Input a number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    let mut guess = String::new();
+
+    loop {
+        guess.clear();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        println!("You guessed: {guess}");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("error");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+    }
+}
