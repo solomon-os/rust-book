42ae1aaa37299661960fa81c246d57fb0f517fc7
Segun Oladiran <sheghunoladiran9@gmail.com>
2026-07-13 23:00:34 +0100

    chapter_5 (structs) done

---
+++ chapter_5/src/main.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rectangle: {rect:#?}");
+
+    let square = Rectangle::square(30);
+    println!("square: {square:#?}");
+}
