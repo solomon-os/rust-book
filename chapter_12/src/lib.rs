6a2c6a7297dd5f1d475002be44ae01799fd1b395
Segun Oladiran <sheghunoladiran9@gmail.com>
2026-07-19 18:32:37 +0100

    chapter 12 done

---
+++ chapter_12/src/lib.rs
@@ -0,0 +1,54 @@
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, duct, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, duct, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insentitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
